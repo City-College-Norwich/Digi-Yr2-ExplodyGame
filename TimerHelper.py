7a7bc7c6f7a8f4995bc58f8c9caf39cf7a93542c
Richard Roth <90181549+Professor-Woody@users.noreply.github.com>
2021-09-30 14:04:48 +0100

    Update TimerHelper.py

--- TimerHelper.py
+++ TimerHelper.py
@@ -3,17 +3,20 @@ import time
 
 class TimerHelper:
     def __init__(self):
-        self.start = time.ticks_ms()
+        self.start = self.getTime()
         self.targetTime = 0
         pass
 
     def set(self, timeInMS):
-        self.start = time.ticks_ms()
+        self.start = self.getTime()
         self.targetTime = timeInMS
 
     def check(self):
-        delta = time.ticks_diff(time.ticks_ms(), self.start)
+        delta = time.ticks_diff(self.getTime(), self.start)
         if delta >= self.targetTime:
             return True
         else:
             return False
+
+    def getTime(self):
+        return round(time.time*1000)
