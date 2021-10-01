bd4fda2de738dd58865904334cc3520b30f6e1e0
Richard Roth <richard.woodward-roth@ccn.ac.uk>
2021-10-01 07:29:42 +0100

    mazetask work

--- MazeTask.py
+++ MazeTask.py
@@ -1,3 +1,42 @@
+# page 19
+# 37: up
+# 27 down
+# 35 right
+# 22 left
+
 class MazeTask:
     def __init__(self, parent):
         self.parent = parent
+
+        # load maze from list of random mazes
+        # choose maze
+        # track the player spot
+        self.map = [[0,0,0,1,0,0,0,0],
+                    [0,1,0,0,0,1,2,0],
+                    [0,0,1,1,0,0,0,0],
+                    [1,0,1,1,0,1,1,1],
+                    [0,0,1,0,0,1,0,0],
+                    [0,1,1,1,0,0,1,0],
+                    [0,1,0,1,1,0,1,0],
+                    [0,0,0,0,0,0,0,0]]
+
+
+
+        print(self.map)
+        self.x = 2
+        self.y = 6
+        
+
+
+
+    def update(self):
+        # check keys
+        # attempt move
+        # check victory
+        direction = self.getKeys()
+
+        pass
+
+    def getKeys(self):
+
+
