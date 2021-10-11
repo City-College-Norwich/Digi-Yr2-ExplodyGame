54ea2325185ff0f0790a3ab6ad340fd87a8bd54b
Richard Roth <90181549+Professor-Woody@users.noreply.github.com>
2021-10-11 16:21:54 +0100

    Update DistanceTask.py

--- DistanceTask.py
+++ DistanceTask.py
@@ -5,10 +5,20 @@ class DistanceTask:
         self.parent = parent
         self.lcd = lcd.Adafruit_CharLCDBackpack(address=0x21)
         
-        self.setBacklight(1)
+        self.setBacklight(True)
         self.setScreen("Hi\nWoody")
 
     def update(self):
+        # if running
+        # if the timer has gone off then randomly generate a new distance (between 3 and 8 as an example)
+        # check if the touch sensor is being pressed
+        # if yes
+            # check if the distance sensor is within 1 inch of the random value
+            # if yes
+                # pass
+                # change state (or add to counter, multiples to pass)
+            # if no
+                # cause strike
         pass
 
     def draw(self):
@@ -23,4 +33,6 @@ class DistanceTask:
         
     def setBacklight(self, value):
         print("setting backlight: {}".format(value))
-        self.lcd.set_backlight(value)
+        
+        # for some reason the library does it backwards.  0 is on and 1 is off
+        self.lcd.set_backlight(not value)
